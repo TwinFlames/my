ccd17de8778b301777a17b5d11e3070550717838
Dark Star☀🎵 <darkstar@unisocial.net>
2022-04-22 04:18:20 -0400

    try

--- ssh/switch.sh
+++ ssh/switch.sh
@@ -3,9 +3,9 @@
 one=twin-flame
 two=twinflames
 
-github=$one
+github=$one #$two
 
 mkdir -p $one $two
 ln -srf $github/known_hosts .
-ln -srf twin-flame/id_rsa
-ln -srf twin-flame/id_rsa.pub .
+ln -srf $github/id_rsa
+ln -srf $github/id_rsa.pub .
